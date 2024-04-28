630b9d88418ebef13b35f83912b00d6f247cd31a
Exidex <16986685+Exidex@users.noreply.github.com>
2024-04-28 15:39:48 +0200

    Fix failing schema load

--- src/grpc.js
+++ src/grpc.js
@@ -1,10 +1,14 @@
 import { load } from "@grpc/proto-loader"
 import { credentials, loadPackageDefinition } from "@grpc/grpc-js"
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // in js file to workaround for https://github.com/grpc/grpc-node/issues/2617
 // also we remove index.d.ts from node_modules/@grpc/grpc-js/build/src/index.d.ts
 export async function setupGrpc() {
-    const packageDefinition = await load('../schema/backend.proto');
+    const packageDefinition = await load(`${__dirname}/../schema/backend.proto`);
     const backend = loadPackageDefinition(packageDefinition);
 
     const client = new backend.RpcBackend("localhost:42320", credentials.createInsecure());
