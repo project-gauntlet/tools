480520d3b63a1179dacbee7ba3948c4be4742b68
Exidex <16986685+exidex@users.noreply.github.com>
2024-12-19 22:01:43 +0100

    Remove workaround for grpc not compiling because it is no longer needed

--- src/grpc.ts
+++ src/grpc.ts
@@ -5,18 +5,16 @@ import { fileURLToPath } from 'node:url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// in js file to workaround for https://github.com/grpc/grpc-node/issues/2617
-// also we remove index.d.ts from node_modules/@grpc/grpc-js/build/src/index.d.ts
 export async function setupGrpc() {
     const packageDefinition = await load(`${__dirname}/../schema/backend.proto`);
-    const backend = loadPackageDefinition(packageDefinition);
+    const backend = loadPackageDefinition(packageDefinition) as any;
 
     const client = new backend.RpcBackend("localhost:42320", credentials.createInsecure());
 
     return {
-        async SaveLocalPlugin(path) {
+        async SaveLocalPlugin(path: string): Promise<{ stdoutFilePath: string, stderrFilePath: string }> {
             return new Promise((resolve, reject) => {
-                client.SaveLocalPlugin({ path }, (err, response) => {
+                client.SaveLocalPlugin({ path }, (err: any, response: any) => {
                     if (err) reject(err);
                     else (resolve(response));
                 });
